e481a15f39f2c90e9c371f1c2d8fb302822ca9a7
Alejandro Laguna <alejandro.laguna@epitech.eu>
2025-08-03 14:46:43 +0200

    chore: home and about page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import astroIcon from "astro-icon"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: { plugins: [tailwindcss()], },
+    integrations: [astroIcon()],
+});
