b70989e7044207871e5de2c0c96c114fa24b60af
Alejandro Laguna <alejandro.laguna@epitech.eu>
2025-08-03 17:25:45 +0200

    ci: fix deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,16 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import astroIcon from "astro-icon"
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: { plugins: [tailwindcss()], },
-    integrations: [astroIcon()],
+  vite: { plugins: [tailwindcss()], },
+  integrations: [astroIcon()],
+  server: {
+    host: '0.0.0.0'
+  },
+  adapter: node({
+    mode: "standalone",
+  }),
 });
