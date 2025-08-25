da6b5ba46e7283291b184f3357444e48cb77e69b
Alejandro Laguna <alejandro.laguna@epitech.eu>
2025-08-25 19:44:50 +0200

    ci: update deploy github

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import astroIcon from "astro-icon"
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   vite: { plugins: [tailwindcss()], },
   integrations: [astroIcon()],
+  site: 'https://alejandrolaguna.dev',
+  base: "/",
+  output: "static",
   server: {
-    host: '0.0.0.0'
+    host: '0.0.0.0',
   },
-  adapter: cloudflare(),
 });
