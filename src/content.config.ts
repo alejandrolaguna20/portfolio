b8afcb77f7e79fe80e845248f7541da557cfaf77
Alejandro Laguna <alejandrolagunaperez20@gmail.com>
2025-11-07 11:53:16 +0100

    fix: rename notes to log

--- src/content.config.ts
+++ src/content.config.ts
@@ -15,4 +15,19 @@ const workshop = defineCollection({
         }),
 });
 
-export const collections = { workshop };
+const log = defineCollection({
+    loader: glob({ base: './src/content/log', pattern: '**/*.{md,mdx}' }),
+    schema: ({ image }) =>
+        z.object({
+            title: z.string(),
+            description: z.string(),
+            pubDate: z.coerce.date(),
+            updatedDate: z.coerce.date().optional(),
+            heroImage: image().optional(),
+            tags: z.array(z.string()).optional(),
+            private: z.boolean().optional()
+        }),
+});
+
+
+export const collections = { workshop, log };
--- src/content/log/tech.md
+++ src/content/log/tech.md
@@ -3,6 +3,7 @@ title: 'My tech'
 description: 'My tech'
 pubDate: '2025-08-03'
 tags: []
+private: true
 ---
 
 Yinyoo KZ ZSN Pro (Blue with mic)
